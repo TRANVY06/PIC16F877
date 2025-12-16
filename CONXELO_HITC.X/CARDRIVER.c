b724d23c48d8a1c9d7f40c00d999fdf2963b2761
TRANVY06 <autohit06@gmail.com>
2025-12-16 16:16:57 +0700

    UPDATE PROJECT CONCARLO _ CARRRDRIVER

--- CONXELO_HITC.X/CARDRIVER.c
+++ CONXELO_HITC.X/CARDRIVER.c
@@ -8,6 +8,16 @@
 #include "Piclb_byNK.h"
 #include <xc.h>
 
-void main(void) {
-    return;
+void Status_Car(unsigned char status, unsigned int sp1, unsigned int sp2) {
+    PORTD = status;
+    analogWrite_8bits(sp1, sp2);
+    /* status==>
+    ==>MoveForward     0x05 //PORTD=0x05 xe tien 
+       MoveBackward    0x0A //PORTD=0x0A xe lùi 
+       TurnRight       0x09 //PORTD=0x09 xe xoay phai 
+       TurnLeft        0x06 //PORTD=0x06 xe xoay trai  
+       MoveRight       0x01 //PORTD=0x01 xe tie  ph?i 
+       MoveLeft        0x04 //PORTD=0x04 xe tien trái
+     */
 }
+
