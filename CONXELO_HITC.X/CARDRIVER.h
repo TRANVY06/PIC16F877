c7ac118abd4319df468c4c3e06a840bea510024b
TRANVY06 <autohit06@gmail.com>
2025-12-16 15:21:11 +0700

    NEW UPDATE

--- CONXELO_HITC.X/CARDRIVER.h
+++ CONXELO_HITC.X/CARDRIVER.h
@@ -1,7 +1,14 @@
-#define MoveForward     0x05 //PORTD=0x05 xe ti?n 
+/*
+ RD7  RB6     RD5              RD4       RD3    RD2   RD1   RD0 
+  x    x    drivercar       autocar      LPWM   RPWM  LPWM  RPWM
+                                         Phai   Phai  Trái  Trái
+ */
+
+#define MoveForward     0x05 //PORTD=0x05 xe tien 
 #define MoveBackward    0x0A //PORTD=0x0A xe lùi 
-#define TurnRight       0x09 //PORTD=0x09 xe qu?o ph?i 
-#define TurnLeft        0x06 //PORTD=0x06 xe qu?o trái  
+#define TurnRight       0x09 //PORTD=0x09 xe xoay phai 
+#define TurnLeft        0x06 //PORTD=0x06 xe xoay trái 
+
 
 extern bool autocar = false;
 extern bool drivercar = false;
